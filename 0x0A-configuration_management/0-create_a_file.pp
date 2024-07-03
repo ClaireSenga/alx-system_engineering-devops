082ff6b70f9a2ffd2e2cc4a4bf32f10b2477a960
ClaireSenga <marieclairemangue2908@gmail.com>
2024-07-03 12:53:03 -0700

    create a file using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 #creating a file in /tmp using puppet
 
 file { '/tmp/school':
-  ensure => 'file',
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
-  content => 'I love Puppet',
+	mode	=> '0744',
+	owner	=> 'www-data',
+	group	=> 'www-data',
+	content => 'I love Puppet',
 }
