fbe273750617ed2d85b42fdb732604397d1dfc40
ClaireSenga <marieclairemangue2908@gmail.com>
2024-08-15 16:25:20 -0700

    num of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """
-A script that queries subscribers on a given Reddit subreddit
+A script that queries subscribers on given Reddit subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-	"""Return the total number of subscribers on given subreddit."""
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {"User-Agent": "Mozilla/5.0"}
-	response = requests.get(url, headers=headers, allow_redirects=False)
-	if response.status_code == 200:
-		data = response.json()
-		subscribers = data['data']['subscribers']
-		return subscribers
-	else:
-		return 0
+    """Return the total num of subscribers on a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0 (compatible; YourScriptName/1.0)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data.get('data', {}).get('subscribers', 0)
+        return subscribers
+    else:
+        # Return 0 if the subreddit is invalid or other errors occur
+        return 0
